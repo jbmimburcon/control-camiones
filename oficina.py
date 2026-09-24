f4650d34b0599ad2fbc85e5478f2f8f70cde6ab1
jbmimburcon <jbm.Imburcon@gmail.com>
2026-09-24 14:53:11 -0400

    Update oficina.py

--- oficina.py
+++ oficina.py
@@ -65,6 +65,7 @@ if opcion_menu == "Formulario de la Secretaria":
         volumen_txt = st.text_input("Volumen transportado (m³):", "0")
         distancia_txt = st.text_input("Distancia del viaje:", "0")
         diesel_txt = st.text_input("Litros de Diésel cargados:", "0")
+        extras_txt = st.text_input("Gastos extras adicionales (Bs):", "0")
         precio_m3_txt = st.text_input("Gastos extras adicionales (Bs):", "0")
         codigo_cfo = st.text_input("Código CFO de la Madera:")
 
